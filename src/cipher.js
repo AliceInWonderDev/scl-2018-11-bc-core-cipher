abf856520c993dd9ce7578e52c02f9004978b033
AliceInWonderdev <ali.castaneda05@gmail.com>
2018-12-04 12:07:04 -0300

    This code works with the cipher file

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
+
+  //declaración de las variables y arreglos vacías 
+  let code = [];
+  let i;
+  let str = [];
+  let newCode;
 window.cipher = {
-  encode: (text, offset) => { 
+ encode: (message, offset) => { 
+  //for: recorrerá el mensaje
+    for(i = 0; i<message.length; i++){
+  //convertiremos el message en ASCII y se guarda en un arreglo
+  str.push((message.charCodeAt(i))-65+offset%26+65);
+  //se convierte el ASCII en letras y se guarda. El push es para agregar
+  code.push(String.fromCharCode(str[i]));
+    }
+  //el método join permite unir elementos de un array en una cadena y los muestra como string
+  newCode = code.join("");
+
+  return newCode;
+  },
+  
+ decode: (message, offset) => { 
+  //recorramos el mensaje con el for
+  for(i = 0; i< message.length; i++){
+  //convertimos el mensaje ASCII y recorré el nivel de dificultad, además se guarda en un arreglo
+  str.push((message.charCodeAt(i))+65-offset%26-65);
+  //Convierte el ASCII en letras y se guarda. El push es para agregar
+  code.push(String.fromCharCode(str[i]));
   }
-  decode: (text, offset) => {  
-  }
+  //coloca el mensaje desencriptado en un recuadro abajo
+  newCode = code.join("");
+  return newCode;
+  }   
+};
