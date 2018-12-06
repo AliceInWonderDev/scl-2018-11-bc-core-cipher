3f05ea115fb09d09bcbb10e8eabb26c18404deb7
AliceInWonderdev <ali.castaneda05@gmail.com>
2018-12-06 11:01:29 -0300

    This project works cipher and all test

--- src/index.js
+++ src/index.js
@@ -1,48 +1,24 @@
-//declaración de las variables y arreglos vacías
-let message;
-let offset;
 
-
-//función que dice al botón codificar qué debe hacer
-function encodeText(){
+//función que dice al botón codificar qué debe hacer: traemos el botón con el ID
+document.getElementById("encode").addEventListener ("click", () =>{
 //message: vamos a guardar el texto ingresado por el usuario
-message = document.getElementById("textoInicial").value;
+let message = document.getElementById("textoInicial").value;
 //con offset vamos a guardar el número de dificultad
-offset = document.getElementById("key").value;
-// resultado
-newCode = cipher.encode(message, offset);
+let offset = document.getElementById("key").value;
+offset = parseInt(offset); 
+
 //coloca el mensaje encriptado en un recuadro
-document.getElementById("descifrado").innerHTML = (newCode.toUpperCase());  
-//toma el root del html
-root.innerHTML= newCode
-};
+document.getElementById("descifrado").innerHTML = (cipher.encode(message, offset));
+}),  
 
-//Función que decodifica texto
-function uncodeText(){
-//message: guarda el texto ingresado por el usuario
-message = document.getElementById("textoInicial").value;
-//offset: guardamos el número de dificultad
-offset = document.getElementById("key").value;
-// resultado
-let newCode = cipher.decode(message, offset);
+//Función que decodifica texto: traemos el boton con el ID
+document.getElementById("uncode").addEventListener("click", () => {
+//message: vamos a guardar el texto ingresado por el usuario
+let message = document.getElementById("textoInicial").value;
+//con offset vamos a guardar el número de dificultad
+let offset = document.getElementById("key").value;
+offset = parseInt(offset); 
+
 //coloca el mensaje encriptado en un recuadro
-document.getElementById("descifrado").innerHTML = (newCode.toUpperCase());
-//toma el root del html
-root.innerHTML= newCode
-};
-
-
-
-/*
-//probar que vuelva a cargar el abcdario
-for(i=0; i<message.length; i++){
- message.push(fromCharCode(message.length));
-}
-
-
-let primeraParte=(message.charCodeAt(i))-97+offset)
-while=(primeraParte < 0){
-  primeraParte = primeraParte + 26
-}
-
-let segundaParte = primeraParte % 26 +97 */
+document.getElementById("descifrado").innerHTML = (cipher.decode(message, offset));
+});
