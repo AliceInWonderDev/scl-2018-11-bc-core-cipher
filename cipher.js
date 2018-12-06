f37132eae23f4b430a2da11817df5b6936ba91a6
AliceInWonderdev <ali.castaneda05@gmail.com>
2018-12-06 11:01:29 -0300

    This project works cipher and all test

--- cipher.js
+++ cipher.js
@@ -1,34 +1,30 @@
-
-  //declaración de las variables y arreglos vacías 
-  let code = [];
-  let i;
-  let str = [];
-  let newCode;
 window.cipher = {
- encode: (message, offset) => { 
-  //for: recorrerá el mensaje
-    for(i = 0; i<message.length; i++){
-  //convertiremos el message en ASCII y se guarda en un arreglo
-  str.push((message.charCodeAt(i))-65+offset%26+65);
-  //se convierte el ASCII en letras y se guarda. El push es para agregar
-  code.push(String.fromCharCode(str[i]));
-    }
-  //el método join permite unir elementos de un array en una cadena y los muestra como string
-  newCode = code.join("");
-
-  return newCode;
-  },
+ encode: (textoInicial, key) => { 
+let texto="";
   
- decode: (message, offset) => { 
-  //recorramos el mensaje con el for
-  for(i = 0; i< message.length; i++){
-  //convertimos el mensaje ASCII y recorré el nivel de dificultad, además se guarda en un arreglo
-  str.push((message.charCodeAt(i))+65-offset%26-65);
-  //Convierte el ASCII en letras y se guarda. El push es para agregar
-  code.push(String.fromCharCode(str[i]));
-  }
-  //coloca el mensaje desencriptado en un recuadro abajo
-  newCode = code.join("");
-  return newCode;
-  }   
-};
+  for(let i = 0; i<textoInicial.length; i++){
+    let str = textoInicial.charCodeAt(i);
+    if (str >=65 && str <=90){
+      let code = (str-65+key)%26+65;
+      texto += String.fromCharCode(code);
+        }
+      }
+        return texto;
+  },
+
+ decode: (textoInicial, key) => { 
+  let texto="";
+    for(let i = 0; i<textoInicial.length; i++){
+      let str = textoInicial.charCodeAt(i);
+        if (str >=65 && str <=90){
+
+      let code = (str-65-key)%26+65;
+          if(code<65){
+            code+=26;
+          }
+       texto += String.fromCharCode(code);
+        }
+      }
+        return texto;
+    }
+      };
