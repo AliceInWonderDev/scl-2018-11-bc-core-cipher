ddf57ad6f3f9eac2685a5a56eae518b022463ecd
AliceInWonderdev <ali.castaneda05@gmail.com>
2018-12-06 21:09:38 -0300

    This code works with numbers, lower case and spaces. Also, this commit have the README

--- cipher.js
+++ cipher.js
@@ -1,30 +1,31 @@
 window.cipher = {
- encode: (textoInicial, key) => { 
-let texto="";
-  
-  for(let i = 0; i<textoInicial.length; i++){
-    let str = textoInicial.charCodeAt(i);
-    if (str >=65 && str <=90){
-      let code = (str-65+key)%26+65;
-      texto += String.fromCharCode(code);
-        }
+  encode: (textoInicial, key) => {
+      let texto = "";
+
+      for (let i = 0; i < textoInicial.length; i++) {
+          let str = textoInicial.charCodeAt(i);
+          if (str >= 65 && str <= 90) {
+              let code = (str - 65 + key) % 26 + 65;
+              texto += String.fromCharCode(code);
+
+          }else{
+            texto += String.fromCharCode(str);
+          }
       }
-        return texto;
+      return texto;
   },
 
- decode: (textoInicial, key) => { 
-  let texto="";
-    for(let i = 0; i<textoInicial.length; i++){
-      let str = textoInicial.charCodeAt(i);
-        if (str >=65 && str <=90){
-
-      let code = (str-65-key)%26+65;
-          if(code<65){
-            code+=26;
+  decode: (textoInicial, key) => {
+      let texto = "";
+      for (let i = 0; i < textoInicial.length; i++) {
+          let str = textoInicial.charCodeAt(i);
+          if (str >= 65 && str <= 90) {
+              let code = (str + 65 - key) % 26 + 65;
+              texto += String.fromCharCode(code);
+          }else{
+            texto += String.fromCharCode(str);
           }
-       texto += String.fromCharCode(code);
-        }
       }
-        return texto;
-    }
-      };
+      return texto;
+  }
+};
--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-
 //función que dice al botón codificar qué debe hacer: traemos el botón con el ID
-document.getElementById("encode").addEventListener ("click", () =>{
-//message: vamos a guardar el texto ingresado por el usuario
-let message = document.getElementById("textoInicial").value;
-//con offset vamos a guardar el número de dificultad
-let offset = document.getElementById("key").value;
-offset = parseInt(offset); 
+document.getElementById("encode").addEventListener("click", () => {
+  //message: vamos a guardar el texto ingresado por el usuario
+  let message = (document.getElementById("textoInicial").value).toUpperCase();
+  //con offset vamos a guardar el número de dificultad
+  let offset = document.getElementById("key").value;
+  offset = parseInt(offset);
 
-//coloca el mensaje encriptado en un recuadro
-document.getElementById("descifrado").innerHTML = (cipher.encode(message, offset));
-}),  
+  //coloca el mensaje encriptado en un recuadro
+  document.getElementById("descifrado").innerHTML = (cipher.encode(message, offset));
+}),
 
 //Función que decodifica texto: traemos el boton con el ID
 document.getElementById("uncode").addEventListener("click", () => {
-//message: vamos a guardar el texto ingresado por el usuario
-let message = document.getElementById("textoInicial").value;
-//con offset vamos a guardar el número de dificultad
-let offset = document.getElementById("key").value;
-offset = parseInt(offset); 
+  //message: vamos a guardar el texto ingresado por el usuario
+  let message = document.getElementById("textoInicial").value.toUpperCase();
+  //con offset vamos a guardar el número de dificultad
+  let offset = document.getElementById("key").value;
+  offset = parseInt(offset);
 
-//coloca el mensaje encriptado en un recuadro
-document.getElementById("descifrado").innerHTML = (cipher.decode(message, offset));
+  //coloca el mensaje encriptado en un recuadro
+  document.getElementById("descifrado").innerHTML = (cipher.decode(message, offset));
 });
